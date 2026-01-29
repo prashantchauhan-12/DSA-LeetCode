d3d60e529fc7e226b79c15f1af3cc933882b295b
prashantchauhan-12 <pr.im12me@gmail.com>
2026-01-29 17:58:39 +0530

    Time: 8 ms (25.45%) | Memory: 23.8 MB (58.95%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -6,20 +6,13 @@ public:
         int n = intervals.size();
 
         for (int i = 0; i < n; i++) {
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-
-            if (!ans.empty() && end <= ans.back()[1])
-                continue;
-
-            for (int j = i + 1; j < n; j++) {
-                if (intervals[j][0] <= end) {
-                    end = max(intervals[j][1], end);
-                } else {
-                    break;
-                }
+            if (i == 0)
+                ans.push_back(intervals[i]);
+            else if (intervals[i][0] <= ans.back()[1]) {
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+            } else {
+                ans.push_back(intervals[i]);
             }
-            ans.push_back({start, end});
         }
         return ans;
     }
