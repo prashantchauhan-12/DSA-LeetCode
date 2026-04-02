98e9252bd33d54afccc45f3760319281589c8188
prashantchauhan-12 <pr.im12me@gmail.com>
2026-04-02 23:49:55 +0530

    Time: 3 ms (9.50%) | Memory: 8.5 MB (34.48%) - LeetSync

---
+++ 3636-check-balanced-string/check-balanced-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isBalanced(string num) {
+        int odd = 0, even = 0;
+        for (int i = 0; i < num.size(); i++) {
+            if (i & 1) {
+                odd += (num[i] - '0');
+            } else {
+                even += (num[i] - '0');
+            }
+        }
+        return odd == even;
+    }
+};
