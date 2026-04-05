4c16fcffac1a5ecdbd26f440451d4c0281fe0448
prashantchauhan-12 <pr.im12me@gmail.com>
2026-04-05 17:36:23 +0530

    Time: 0 ms (100.00%) | Memory: 27.8 MB (94.15%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -1,24 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int maxSum = INT_MIN;
-
-    int dfs(TreeNode* node) {
-        if (node == NULL)
+    int maxi = INT_MIN;
+    int f(TreeNode* root) {
+        if (root == NULL)
             return 0;
 
-        // Ignore negative contributions
-        int left = max(0, dfs(node->left));
-        int right = max(0, dfs(node->right));
+        int left = max(0, f(root->left));
+        int right = max(0, f(root->right));
 
-        // Update global maximum (path passing through this node)
-        maxSum = max(maxSum, left + right + node->val);
+        // through root
+        maxi = max(maxi, root->val + left + right);
 
-        // Return one side path (for parent use)
-        return node->val + max(left, right);
+        return root->val + max(left, right);
     }
-
     int maxPathSum(TreeNode* root) {
-        dfs(root);
-        return maxSum;
+        f(root);
+        return maxi;
     }
 };
