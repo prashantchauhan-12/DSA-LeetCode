b172efd61ae5c985ca6a9e7945462ffde4550a62
prashantchauhan-12 <pr.im12me@gmail.com>
2026-04-05 17:15:28 +0530

    Time: 0 ms (100.00%) | Memory: 27.8 MB (94.15%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
+    int maxSum = INT_MIN;
 
-    // Get max path starting from this node (one direction only)
-    int getPathSum(TreeNode* root) {
-        if (!root) return 0;
+    int dfs(TreeNode* node) {
+        if (node == NULL)
+            return 0;
 
-        int left = max(0, getPathSum(root->left));
-        int right = max(0, getPathSum(root->right));
+        // Ignore negative contributions
+        int left = max(0, dfs(node->left));
+        int right = max(0, dfs(node->right));
 
-        return root->val + max(left, right);
-    }
+        // Update global maximum (path passing through this node)
+        maxSum = max(maxSum, left + right + node->val);
 
-    // Try every node as root of path
-    int solve(TreeNode* root) {
-        if (!root) return INT_MIN;
-
-        // path passing through this node
-        int left = max(0, getPathSum(root->left));
-        int right = max(0, getPathSum(root->right));
-        int throughRoot = root->val + left + right;
-
-        // check in left and right subtree
-        int leftAns = solve(root->left);
-        int rightAns = solve(root->right);
-
-        return max({throughRoot, leftAns, rightAns});
+        // Return one side path (for parent use)
+        return node->val + max(left, right);
     }
 
     int maxPathSum(TreeNode* root) {
-        return solve(root);
+        dfs(root);
+        return maxSum;
     }
 };
