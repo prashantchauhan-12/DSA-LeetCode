6c8c2814c0e2e168f2ffd1bc87cd21e9bba8b27b
prashantchauhan-12 <pr.im12me@gmail.com>
2026-04-14 13:12:23 +0530

    Time: 47 ms (15.05%) | Memory: 47.8 MB (19.36%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-
         // Step 1: Count frequency of each task
         unordered_map<char, int> mp;
         for (char t : tasks) {
             mp[t]++;
         }
 
-        // Step 2: Max heap to always pick the task with highest remaining frequency
+        // Step 2: Max heap to always pick the task with highest remaining
+        // frequency
         priority_queue<int> maxHeap;
         for (auto it : mp) {
             maxHeap.push(it.second);
         }
 
         int time = 0;
-
         // Step 3: Process tasks in cycles of size (n + 1)
-        // Each cycle represents the minimum gap required between same tasks
         while (!maxHeap.empty()) {
 
-            vector<int> temp;   // store tasks that still have remaining count after this cycle
-            int cycle = n + 1;  // max distinct tasks we can schedule in one cycle
-            int tasksCount = 0; // actual tasks executed in this cycle
+            vector<int> temp;
+            int cycle = n + 1;
+            int tasksCount = 0;
 
-            // Step 4: Try to fill the cycle with most frequent tasks
             for (int i = 0; i < cycle; i++) {
 
                 if (!maxHeap.empty()) {
@@ -36,25 +33,14 @@ public:
                         temp.push_back(freq - 1);
                     }
 
-                    tasksCount++; // one task executed
+                    tasksCount++;
                 }
             }
 
-            // Step 5: Push remaining tasks back into heap
             for (int remaining : temp) {
                 maxHeap.push(remaining);
             }
 
-            // Step 6: Update total time
-            /*
-                Case 1: Heap is empty
-                → No tasks left → no need to add idle time
-                → Only count actual tasks executed
-
-                Case 2: Heap NOT empty
-                → Still tasks remaining but cycle couldn't be fully filled
-                → We must account for idle slots → full cycle time
-            */
             if (maxHeap.empty()) {
                 time += tasksCount;
             } else {
