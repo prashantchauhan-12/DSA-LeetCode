3754d828b12d9dab608f6389bada474de5a62b9c
prashantchauhan-12 <pr.im12me@gmail.com>
2026-04-14 13:21:38 +0530

    Time: 0 ms (100.00%) | Memory: 38.2 MB (95.20%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,53 +1,23 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        // Step 1: Count frequency of each task
-        unordered_map<char, int> mp;
-        for (char t : tasks) {
-            mp[t]++;
+        vector<int> freq(26, 0);
+        for (char c : tasks)
+            freq[c - 'A']++;
+
+        sort(freq.begin(), freq.end());
+
+        int maxf = freq[25];
+        int time = (maxf - 1) * (n + 1);
+        time++;
+
+        for (int i = 24; i >= 0; i--) {
+            if (freq[i] == maxf)
+                time++;
+            else
+                break;
         }
 
-        // Step 2: Max heap to always pick the task with highest remaining
-        // frequency
-        priority_queue<int> maxHeap;
-        for (auto it : mp) {
-            maxHeap.push(it.second);
-        }
-
-        int time = 0;
-        // Step 3: Process tasks in cycles of size (n + 1)
-        while (!maxHeap.empty()) {
-
-            vector<int> temp;
-            int cycle = n + 1;
-            int tasksCount = 0;
-
-            for (int i = 0; i < cycle; i++) {
-
-                if (!maxHeap.empty()) {
-                    int freq = maxHeap.top();
-                    maxHeap.pop();
-
-                    // Execute this task → reduce its frequency
-                    if (freq - 1 > 0) {
-                        temp.push_back(freq - 1);
-                    }
-
-                    tasksCount++;
-                }
-            }
-
-            for (int remaining : temp) {
-                maxHeap.push(remaining);
-            }
-
-            if (maxHeap.empty()) {
-                time += tasksCount;
-            } else {
-                time += cycle;
-            }
-        }
-
-        return time;
+        return max((int)tasks.size(), time);
     }
 };
