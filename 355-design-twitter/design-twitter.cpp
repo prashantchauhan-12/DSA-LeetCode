801b7eee6c35d47290c594e9319befa9da9150bb
prashantchauhan-12 <pr.im12me@gmail.com>
2026-04-14 18:04:30 +0530

    Time: 9 ms (46.12%) | Memory: 55.8 MB (69.91%) - LeetSync

---
+++ 355-design-twitter/design-twitter.cpp
@@ -0,0 +1,69 @@
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+class Twitter {
+private:
+    // Global timer to simulate tweet recency
+    int timestamp;
+
+    // userId -> list of pairs {timestamp, tweetId}
+    unordered_map<int, vector<pair<int, int>>> tweets;
+
+    // userId -> set of followeeIds
+    unordered_map<int, unordered_set<int>> following;
+
+public:
+    Twitter() { timestamp = 0; }
+
+    void postTweet(int userId, int tweetId) {
+        // Store tweet with global timestamp
+        tweets[userId].push_back({timestamp++, tweetId});
+    }
+
+    vector<int> getNewsFeed(int userId) {
+        // Min-heap to keep track of the 10 most recent tweets
+        // Stores: pair<timestamp, tweetId>
+        priority_queue<pair<int, int>, vector<pair<int, int>>,
+                       greater<pair<int, int>>>
+            pq;
+
+        // 1. Get user's own tweets
+        for (auto& t : tweets[userId]) {
+            pq.push(t);
+            if (pq.size() > 10)
+                pq.pop();
+        }
+
+        // 2. Get tweets from everyone they follow
+        for (int followeeId : following[userId]) {
+            for (auto& t : tweets[followeeId]) {
+                pq.push(t);
+                if (pq.size() > 10)
+                    pq.pop();
+            }
+        }
+
+        // 3. Extract from heap (results will be in increasing order of time)
+        vector<int> res;
+        while (!pq.empty()) {
+            res.insert(res.begin(), pq.top().second);
+            pq.pop();
+        }
+        return res;
+    }
+
+    void follow(int followerId, int followeeId) {
+        // A user cannot follow themselves in this context (usually)
+        if (followerId != followeeId) {
+            following[followerId].insert(followeeId);
+        }
+    }
+
+    void unfollow(int followerId, int followeeId) {
+        if (following[followerId].count(followeeId)) {
+            following[followerId].erase(followeeId);
+        }
+    }
+};
