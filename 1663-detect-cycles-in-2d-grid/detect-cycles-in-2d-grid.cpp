bd710b5c52150dfe9d0c679ca52a9490c6f1447a
prashantchauhan-12 <pr.im12me@gmail.com>
2026-04-26 11:50:31 +0530

    Time: 23 ms (78.23%) | Memory: 67 MB (68.14%) - LeetSync

--- 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.cpp
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.cpp
@@ -2,6 +2,29 @@ class Solution {
 public:
     int m, n;
     vector<pair<int, int>> dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+    bool dfs(int x, int y, int px, int py, vector<vector<char>>& grid,
+             vector<vector<bool>>& visited) {
+        visited[x][y] = true;
+
+        for (auto dir : dirs) {
+            int nx = x + dir.first;
+            int ny = y + dir.second;
+
+            if (nx >= 0 && nx < m && ny >= 0 && ny < n &&
+                grid[nx][ny] == grid[x][y]) {
+                if (!visited[nx][ny]) {
+                    if (dfs(nx, ny, x, y, grid, visited)) {
+                        return true;
+                    }
+                } else if (!(nx == px && ny == py)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     bool containsCycle(vector<vector<char>>& grid) {
         m = grid.size();
         n = grid[0].size();
@@ -10,33 +33,8 @@ public:
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (!visited[i][j]) {
-
-                    queue<pair<pair<int, int>, pair<int, int>>> q;
-                    q.push({{i, j}, {-1, -1}});
-                    visited[i][j] = true;
-
-                    while (!q.empty()) {
-                        auto [current, parent] = q.front();
-                        q.pop();
-
-                        int x = current.first;
-                        int y = current.second;
-
-                        for (auto dir : dirs) {
-                            int nx = x + dir.first;
-                            int ny = y + dir.second;
-
-                            if (nx >= 0 && nx < m && ny >= 0 && ny < n &&
-                                grid[nx][ny] == grid[x][y]) {
-                                if (!visited[nx][ny]) {
-                                    visited[nx][ny] = true;
-                                    q.push({{nx, ny}, {x, y}});
-                                } else if (!(nx == parent.first &&
-                                             ny == parent.second)) {
-                                    return true;
-                                }
-                            }
-                        }
+                    if (dfs(i, j, -1, -1, grid, visited)) {
+                        return true;
                     }
                 }
             }
